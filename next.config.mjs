743583695a1157eee087951e2d7c145246da3617
Macbook Pro <macbookpro@Macbooks-MacBook-Pro.local>
2026-06-01 11:35:15 +0700

    fix: prevent redirect indexing issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  trailingSlash: false,
   images: {
     formats: ['image/webp', 'image/avif'],
     deviceSizes: [640, 750, 828, 1080, 1200],
